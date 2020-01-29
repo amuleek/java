2332393a5fed757880c368cfaa9b31b8e90d9b2e
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-01-29 18:50:35 +0530

    Create DefaultConstructor.java

---
+++ DefaultConstructor.java
@@ -0,0 +1,13 @@
+class DefaultConstructor {
+
+    int a;
+    boolean b;
+
+    public static void main(String[] args) {
+
+        DefaultConstructor obj = new DefaultConstructor();
+
+        System.out.println("a = " + obj.a);
+        System.out.println("b = " + obj.b);
+    }
+}
