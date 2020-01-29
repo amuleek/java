b772eee1d115e0d3409fb853f1b0408c26a33bcb
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-01-29 18:38:56 +0530

    Create lamp.java

---
+++ lamp.java
@@ -0,0 +1,15 @@
+class Lamp {
+
+  // instance variable
+  private boolean isOn;
+
+  // method
+  public void turnOn() {
+    isOn = true;
+  }
+
+  // method
+  public void turnOff() {
+  	isOn = false;
+  }
+}
