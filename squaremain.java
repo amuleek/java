81e37b2d389f3930129c792d554ac8053b68a7c7
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-01-29 18:43:25 +0530

    Create squaremain.java

---
+++ squaremain.java
@@ -0,0 +1,13 @@
+lass SquareMain {
+    public static void main(String[] args) {
+        int result;
+        result = square(); 
+        System.out.println("Squared value of 10 is: " + result);
+    }
+
+          public static int square() {
+        // return statement
+        return 10 * 10;
+    }
+
+}
