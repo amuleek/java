64b40d3402866573614502cb168c3bcff64d6882
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-05-06 20:37:02 +0530

    Create Integerliterals.java

---
+++ Integerliterals.java
@@ -0,0 +1,13 @@
+public class Test { 
+    public static void main(String[] args) 
+    { 
+        int a = 101; // decimal-form literal 
+        int b = 0100; // octal-form literal 
+        int c = 0xFace; // Hexa-decimal form literal 
+        int d = 0b1111; // Binary literal 
+        System.out.println(a); 
+        System.out.println(b); 
+        System.out.println(c); 
+        System.out.println(d); 
+    } 
+} 
