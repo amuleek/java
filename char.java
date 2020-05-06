78fada394fb5062dc3ddf78f8e86aa0350f28aaf
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-05-06 22:47:38 +0530

    Create char.java

---
+++ char.java
@@ -0,0 +1,14 @@
+// Java program to illustrate the application of char literals 
+public class Test { 
+public static void main(String[] args) 
+    { 
+        char ch = 'a'; // signle character literl within signle quote 
+        char b = 0789; // It is an Integer literal with octal form 
+        char c = '\u0061'; // Unicode representation 
+        System.out.println(ch); 
+        System.out.println(b); 
+        System.out.println(c); 
+        // Escape character literal 
+        System.out.println("\"  is a symbol"); 
+    } 
+} 
