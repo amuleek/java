8c130dbf18a09a0c349fde774b3aad49aae8c52e
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-05-06 20:52:01 +0530

    Create floating-pointliterals.java

---
+++ floating-pointliterals.java
@@ -0,0 +1,11 @@
+public class Test { 
+    public static void main(String[] args) 
+    { 
+        float a = 101.230; // decimal-form literal 
+        float b = 0123.222; // It also acts as decimal literal 
+        float c = 0x123.222; // Hexa-decimal form 
+        System.out.println(a); 
+        System.out.println(b); 
+        System.out.println(c); 
+    } 
+} 
