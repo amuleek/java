99c5d9fd74eaab41c404c29ef91c906528234dc6
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-05-27 12:22:32 +0530

    Create psvm.java

---
+++ psvm.java
@@ -0,0 +1,15 @@
+/ Java program to illustrate the behaviour of 
+// char literals and integer literals when 
+// we are performing addition 
+public class Test { 
+    public static void main(String[] args) 
+    { 
+        // ASCII value of 0 is 48 
+        int first = '0'; 
+  
+        // ASCII value of 7 is 55 
+        int second = '7'; 
+        System.out.println("Geeks!" + first + 
+                                '2' + second); 
+    } 
+} 
