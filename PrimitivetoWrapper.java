d1cf0331bffab319128c3f6ad44ac756804c1655
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-06-02 18:50:27 +0530

    Update PrimitivetoWrapper.java

--- PrimitivetoWrapper.java
+++ PrimitivetoWrapper.java
@@ -1,3 +1,6 @@
 //Java program to convert primitive into objects  
 //Autoboxing example of int to Integer  
 public class Ex1{  
+public static void main(String args[]){  
+//Converting int into Integer  
+int a=20;  
