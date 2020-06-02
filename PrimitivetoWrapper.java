8d5bc0347c36ff55806753729a0915ef51edcede
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-06-02 18:57:33 +0530

    Update PrimitivetoWrapper.java

--- PrimitivetoWrapper.java
+++ PrimitivetoWrapper.java
@@ -5,4 +5,6 @@ public static void main(String args[]){
 //Converting int into Integer  
 int a=20;  
 Integer i=Integer.valueOf(a);//converting int into Integer explicitly  
-Integer j=a;//autoboxing, now compiler will write Integer.valueOf(a) internally  
+Integer j=a;//autoboxing, now compiler will write Integer.valueOf(a) internally 
+  System.out.println(a+" "+i+" "+j);  
+}}  
